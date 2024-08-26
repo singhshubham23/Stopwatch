f9be6492da42857d6e0191cc0ccfe3e1a83ecca1
Shubham Kumar <142138865+singhshubham23@users.noreply.github.com>
2024-08-26 16:54:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+let [miliseconds, seconds, minutes, hours] = [0,0,0,0];
+let timeRef = document.querySelector(".timer-display");
+let int = null;
+
+
+document.getElementById("start-timer").addEventListener("click", () =>{
+    if (int !== null) {
+        clearInterval(int);
+    }
+    int = setInterval(displayTimer, 10);
+});
+
+document.getElementById("pause-timer").addEventListener("click", () =>{
+    clearInterval(int);
+});
+
+document.getElementById("reset-timer").addEventListener("click", () =>{
+    clearInterval(int);
+    [miliseconds, seconds, minutes, hours] = [0,0,0,0];
+    timeRef.innerHTML = "00 : 00 : 00 : 000";
+});
+
+function displayTimer(params) {
+    miliseconds += 10;
+    if (miliseconds == 1000) {
+        miliseconds = 0;
+        seconds++;
+    }
+    if (seconds == 60) {
+        seconds = 0;
+        minutes++;
+    }
+    if (minutes == 60) {
+        minutes = 0;
+        hours++;
+    }
+
+    let h = hours < 10 ? "0" + hours:hours;
+    let m = minutes < 10 ? "0" + minutes:minutes;
+    let s = seconds < 10 ? "0" + seconds:seconds;
+    let ms = miliseconds < 10 ? "00" + miliseconds:miliseconds < 100 ? "0" + miliseconds:miliseconds;
+
+    timeRef.innerHTML = `${h} : ${m} : ${s} : ${ms}`;
+
+}
+const onButton = document.getElementById("on-button");
+const offButton = document.getElementById("off-button");
+
+onButton.addEventListener("click", () => {
+    document.body.classList.add("dark-mode");
+});
+
+offButton.addEventListener("click", () => {
+    document.body.classList.remove("dark-mode");
+});
+
